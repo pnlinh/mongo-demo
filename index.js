7458dbcf5b8f11f62bd204a1fbb21985b8b9639f
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-03-23 10:20:32 +0700

    count query

--- index.js
+++ index.js
@@ -56,7 +56,8 @@ async function getCourses() {
         .find({ author: /.*Mosh.*/i })
         .limit(10)
         .sort({name: 1})
-        .select({name: 1, tags: 1});
+        .select({name: 1, tags: 1})
+        .count();
 
     console.log(courses);
 }
