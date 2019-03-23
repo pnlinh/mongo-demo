28825a9983f8d02cb827b2ec4d374ea67a595c5f
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-03-23 09:51:18 +0700

    logical query operator

--- index.js
+++ index.js
@@ -38,11 +38,16 @@ async function getCourses() {
     // lte (less than or equal to)
     // in
     // nin(not in)
+    // or
+    // and
 
     const courses = await Course
         // .find({author: 'Mosh', isPublished: true})
         // .find({price: { $gte: 10, $lte: 20 }})
-        .find({price: { $in: [10, 15, 20] }})
+        // .find({price: { $in: [10, 15, 20] }})
+        .find()
+        .or([{author: 'Mosh'}, {isPublished: true}])
+        .and([])
         .limit(10)
         .sort({name: 1})
         .select({name: 1, tags: 1});
